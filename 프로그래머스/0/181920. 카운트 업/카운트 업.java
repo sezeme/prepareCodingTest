095800e2646d1cae53a43694f31d25497884cae5
sezeme <stellaa223@gmail.com>
2025-01-24 20:01:56 +0900

    [level 0] Title: 카운트 업, Time: 2.25 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,8 @@
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = IntStream.rangeClosed(start_num, end_num).toArray();
+        return answer;
+    }
+}
