87f4fa670f5a012e4416606af499d1a6409b1cb3
sezeme <stellaa223@gmail.com>
2025-02-18 14:15:12 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,13 +3,11 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        String s = "" + n;
-        int size = s.length();
         
-        for(int i = 0; i<size; i++){
-            answer += s.charAt(i) - '0';
+        while(true) {
+            answer += n % 10;
+            if(n < 10) return answer;
+            n /= 10;
         }
-
-        return answer;
     }
 }
