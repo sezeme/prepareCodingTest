6873e1440cdd557cedfc55dfc2063e38ffdf1bf5
sezeme <stellaa223@gmail.com>
2025-02-20 10:46:29 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean solution(int x) {
+        String s = Integer.toString(x);
+        int sum = 0;
+        for(int i = 0; i<s.length() ; i++){
+            sum += s.charAt(i) - '0';
+        }
+        if(x % sum == 0) return true;
+        return false;
+    }
+}
