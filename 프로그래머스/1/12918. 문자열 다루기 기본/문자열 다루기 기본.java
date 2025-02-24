8a75755d1070fbe621b00448806917704906f671
sezeme <stellaa223@gmail.com>
2025-02-25 01:38:36 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean solution(String s) {
+        if(s.length() != 4 && s.length() != 6) return false;
+        for(int i = 0; i<s.length() ; i++){
+            char c = s.charAt(i);
+            if(c >= '0' && c <= '9');
+            else return false;
+        }
+        return true;
+    }
+}
