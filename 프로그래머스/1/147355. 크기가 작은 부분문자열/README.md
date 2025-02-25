fd263f07dd91481aab43f9a8403bb280010d8048
sezeme <stellaa223@gmail.com>
2025-02-25 18:43:38 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 7.70 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/README.md
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크기가 작은 부분문자열 - 147355 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147355) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147355#) 
 
 ### 성능 요약
 
-메모리: 70 MB, 시간: 2.61 ms
+메모리: 88.9 MB, 시간: 7.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 25일 18:41:50
+2025년 02월 25일 18:43:36
 
 ### 문제 설명
 
--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,19 +1,14 @@
 class Solution {
     public int solution(String t, String p) {
         int answer = 0;
-        for(int i = 0; i<=t.length() - p.length(); i++){
-            int result = comparator(t.substring(i,i+p.length()), p);
-            if(result <= 0) answer++;
+        long ip = Long.parseLong(p);
+
+        for(int i=0;i<=t.length()-p.length();i++){
+            if(Long.parseLong(t.substring(i,i+p.length()))<=ip){
+                answer++;
+            }
         }
+
         return answer;
-    }
-    
-    int comparator(String tSubStr, String p) {
-        for(int i = 0; i< tSubStr.length(); i++){
-            int sub = tSubStr.charAt(i) - p.charAt(i);
-            if(sub == 0) continue;
-            return sub;
-        }
-        return 0;
     }
 }
