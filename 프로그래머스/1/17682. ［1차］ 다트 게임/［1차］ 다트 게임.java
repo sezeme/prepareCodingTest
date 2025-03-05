4fe0b26726ab69167b6c6531056ffd3b870b598b
sezeme <stellaa223@gmail.com>
2025-03-05 15:27:27 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.28 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,39 +1,36 @@
+import java.util.*;
+
 class Solution {
     public int solution(String dartResult) {
         int answer = 0;
-        int prevNum = 0;
+        Stack<Integer> stack = new Stack<>();
         
         for(int i = 0; i < dartResult.length() ; i++){
-            // 점수
-            char ch = dartResult.charAt(i++);
-            int num;
-            if(ch == '1' && dartResult.charAt(i) == '0'){ i++; num = 10; }
-            else num = ch - '0';
-            
-            // 보너스
-            ch = dartResult.charAt(i);
-            if(ch == 'S');
-            else if(ch == 'D') num = (int)Math.pow(num, 2);
-            else if(ch == 'T') num = (int)Math.pow(num, 3);
-            
-            // 옵션
-            if(i + 1 < dartResult.length()){
-                ch = dartResult.charAt(i+1);
-                if(ch == '*') {
+            char ch = dartResult.charAt(i);
+            if(Character.isDigit(ch)){
+                // 점수
+                int num = ch - '0';
+                if(num == 1 && i < dartResult.length() -1 && dartResult.charAt(i+1) == '0'){ i++; num = 10; }
+                stack.push(num);
+            } else {
+                // 보너스 or 옵션
+                int num = stack.pop();
+                if(ch == 'S');
+                else if(ch == 'D') num = (int)Math.pow(num, 2);
+                else if(ch == 'T') num = (int)Math.pow(num, 3);
+                else if(ch == '*') {
+                    if(!stack.isEmpty()) stack.push(stack.pop() * 2);
                     num *= 2;
-                    prevNum *= 2;
-                    i++;
                 }
-                else if(ch == '#') {
-                    num *= -1;
-                    i++;
-                }
+                else if(ch == '#') num *= -1;
+                
+                stack.push(num);
             }
-            
-            answer += prevNum;
-            prevNum = num;
         }
-        answer += prevNum;
+        while(!stack.isEmpty()){
+            answer += stack.pop();
+        }
+        
         return answer;
     }
 }
