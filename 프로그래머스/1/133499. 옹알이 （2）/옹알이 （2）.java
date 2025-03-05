e3886961ec733f7fdd8fec428979291c29e46a9c
sezeme <stellaa223@gmail.com>
2025-03-05 14:05:23 +0900

    [level 1] Title: 옹알이 (2), Time: 1.97 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -3,26 +3,15 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         for(String str : babbling) {
-            if(isBabble(str)) answer++;
+            if(str.contains("ayaaya") || str.contains("yeye") || str.contains("woowoo") || str.contains("mama"))continue;
+            
+            for(String str2 : list) {
+                str = str.replace(str2, " ");
+            }
+            str = str.replace(" ", "");
+            
+            if(str.equals("")) answer++;
         }
         return answer;
-    }
-    
-    static boolean isBabble(String babble) {
-        boolean flag;
-        String prevString = "";
-        for(int i = 0; i< babble.length(); i++){
-            flag = false;
-            for(int j = 0; j < 4 ; j++){
-                if(i+list[j].length() - 1 < babble.length() && list[j].equals(babble.substring(i,i+list[j].length())) && !prevString.equals(babble.substring(i, i+ list[j].length()))){
-                    i += list[j].length() - 1;
-                    prevString = list[j];
-                    break;
-                }
-                if(j == 3) flag = true;
-            }
-            if(flag) return false;
-        }
-        return true;
     }
 }
