634bee002cf16c02a291958826d7acb4278341d2
Chewon Kim <stellaa223@gmail.com>
2025-06-09 18:53:51 +0900

    [Bronze IV] Title: 새천년관, Time: 92 ms, Memory: 13788 KB -BaekjoonHub

---
+++ 백준/Bronze/33701. 새천년관/새천년관.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Main {
+    static String str;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("nkugwan");
+
+
+    }
+}
