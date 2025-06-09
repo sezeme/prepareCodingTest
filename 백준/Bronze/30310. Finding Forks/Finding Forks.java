b5038192dfe7a64c8f23e9293184dd7705155243
Chewon Kim <stellaa223@gmail.com>
2025-06-09 19:01:28 +0900

    [Bronze III] Title: Finding Forks, Time: 396 ms, Memory: 32972 KB -BaekjoonHub

---
+++ 백준/Bronze/30310. Finding Forks/Finding Forks.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        
+        Arrays.sort(arr);
+
+        System.out.println(arr[0] + arr[1]);
+        
+        
+
+    }
+}
