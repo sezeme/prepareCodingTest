f35393e4ea689e8e6b521707315144a1b6e16b62
Chewon Kim <stellaa223@gmail.com>
2025-06-13 22:42:45 +0900

    [Silver V] Title: 방 번호, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,33 +1,32 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
+    static String N;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String set = br.readLine();
-
-        // 9는 6과 함께 count 후 /2
-        int[] array = new int[10];
-
-        for(int i = 0; i < set.length() ; i++) {
-            int index = set.charAt(i) - '0';
-            if(index == 9) index = 6;
-
-            array[index]++;
+        N = br.readLine();
+        arr = new int[10];
+        
+        for(int i = 0; i < N.length() ; i++){
+            char ch = N.charAt(i);
+            arr[ch - '0']++;
         }
-
-        int max = 0;
-        for(int i = 0; i < 9 ; i++) {
-            if(i == 6) {
-                if((array[i]+1)/2 > max) max = (array[i]+1)/2;
-            }
-            else if(array[i] > max) max = array[i];
+        
+        int sum = arr[9] + arr[6];
+        if(sum%2 == 0){
+            arr[9] = arr[6] = sum/2;
+        } else {
+            arr[9] = sum/2 + 1;
+            arr[6] = sum/2;
         }
+        
+        Arrays.sort(arr);
+        
+        System.out.println(arr[9]);
 
-        System.out.println(max);
     }
 }
