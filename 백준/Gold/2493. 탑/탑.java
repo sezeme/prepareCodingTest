d27aa7904706dfa77cbbfbf5083fa553cf034614
Chewon Kim <stellaa223@gmail.com>
2025-06-17 20:31:39 +0900

    [Gold V] Title: 탑, Time: 664 ms, Memory: 83908 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,5 +1,3 @@
-// BOJ_2493  : 탑
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,34 +6,37 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static int[] arr, solution;
     static Stack<Integer> stk;
-
+    static int[] arr, answer;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        solution = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[N + 1];
+        answer = new int[N + 1];
         stk = new Stack<>();
 
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i<N; i++){
+        for(int i = 1; i <= N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        for(int i = N-1; i>= 0;i--) {
-            while (!stk.isEmpty() && arr[stk.peek()] < arr[i]) {
-                int n = stk.pop();
-                solution[n] = i + 1;
+        
+        int received_tower = 1;
+        for(int i = N; i >= 1; i--){
+            if(stk.isEmpty()) stk.push(i);
+            else {
+                while(!stk.isEmpty() && arr[stk.peek()] < arr[i]) {
+                    answer[stk.pop()] = i;
+                }
+                stk.push(i);
             }
-            stk.push(i);
         }
+        
 
-        for(int i = 0; i<solution.length; i++){
-            if(i+1 == solution.length)
-                System.out.println(solution[i]);
-            else System.out.print(solution[i]+" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= N; i++){
+            sb.append(answer[i]).append(" ");
         }
+        System.out.println(sb);
     }
 }
+
