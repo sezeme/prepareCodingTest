9a125fe541e43f86d2b57b8ba4c4fc9211d03c21
Chewon Kim <stellaa223@gmail.com>
2025-06-19 01:00:08 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 45500 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,30 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Queue;
 import java.util.LinkedList;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-
 public class Main {
-
+    static int N;
+    static Queue<Integer> q;
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        Queue<Integer> q = new LinkedList<>();
-
-        int N = Integer.parseInt(br.readLine());
-
-        for(int i = 1; i <= N; i++) {
+        N = Integer.parseInt(br.readLine());
+        
+        q = new LinkedList<>();
+        for(int i = 1; i <= N; i++){
             q.offer(i);
         }
-
-
-        while(q.size() > 1) {
-            q.poll();	// 맨 앞의 원소 버림
-            q.offer(q.poll());	// 맨 앞의 원소를 버림과 동시에 버려진 원소를 맨 뒤에 삽입
+        
+        while(q.size() != 1){
+            q.poll();
+            q.offer(q.poll());
         }
-
-        System.out.println(q.poll());	// 마지막으로 남은 원소 출력
+        
+        System.out.println(q.poll());
     }
 }
